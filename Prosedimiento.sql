f5624a64d82ecdb25a70d2d66127bd38b996b350
anapse <74956720+anapse@users.noreply.github.com>
2024-08-14 23:09:37 -0500

    Update Prosedimiento.sql
    reparacion id64

--- Prosedimiento.sql
+++ Prosedimiento.sql
@@ -33,6 +33,6 @@ as
  
 	if (@Plus_Item >= @Plus_Req and @Active != 0) 
 		BEGIN     
-			UPDATE	SRO_VT_SHARD.._Items Set RefItemID= @ID_Upgrade ,OptLevel = 0 ,MagParamNum = 0 , Variance = 0,MagParam1=0,MagParam2=0,MagParam3=0,MagParam4=0,MagParam5=0,MagParam6=0,MagParam7=0,MagParam8=0,MagParam9=0,MagParam10=0
+			UPDATE	SRO_VT_SHARD.._Items Set RefItemID= @ID_Upgrade ,OptLevel = 0 ,MagParamNum = 0 , Variance = 0,MagParam1=0,MagParam2=0,MagParam3=0,MagParam4=0,MagParam5=0,MagParam6=0,MagParam7=0,MagParam8=0,MagParam9=0,MagParam10=0 where ID64 = @ID64    
 		END
 	End
